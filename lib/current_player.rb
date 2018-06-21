ac960604eef2925d980f2bc373581532c8a73fd5
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-06-21 15:23:02 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,4 +1,13 @@
-def turn_count
+def turn_count(board)
+  board.each do |turn|
+    player_1 = 0
+    player_2 = 0
+    
+    if turn == "X"
+      player_1 += 1
+    elsif turn == "O"
+      player_2 += 1
+  end
 end
 
 def current_player
